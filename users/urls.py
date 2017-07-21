8f78a597bb3a399b3150f108e42a6de6069a6316
ali nemati chari <alinemati@live.com>
2017-07-21 21:56:50 +0430

    a

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ import users.views
 __author__ = 'ali-user'
 
 urlpatterns = [
-    url(r'^register', users.views.register, name='register'),
-    url(r'^login', users.views.login, name='login'),
-    url(r'^blog_id',users.views.blog_id, name='blog_id'),
+    url(r'^register/', users.views.register, name='register'),
+    url(r'^login/', users.views.login, name='login'),
+    url(r'^blog_id/',users.views.blog_id, name='blog_id'),
 ]
